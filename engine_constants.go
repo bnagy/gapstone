855c651b22ad40bca3aa962816a224f5774d6e96
Ben <ben@coseinc.com>
2013-11-28 12:41:45 +1200

    update constant declarations to more closely match C headers

--- engine_constants.go
+++ engine_constants.go
@@ -1,26 +1,36 @@
 package gapstone
 
-const CS_ARCH_ARM = 0   // ARM architecture (including Thumb Thumb-2)
-const CS_ARCH_ARM64 = 1 // ARM-64, also called AArch64
-const CS_ARCH_MIPS = 2  // Mips architecture
-const CS_ARCH_X86 = 3   // X86 architecture (including x86 & x86-64)
-const CS_ARCH_MAX = 4
-
-const CS_MODE_LITTLE_ENDIAN = 0    // little endian mode (default mode)
-const CS_MODE_SYNTAX_INTEL = 0     // Intel X86 asm syntax (CS_ARCH_X86 architecture)
-const CS_MODE_ARM = 0              // 32-bit ARM
-const CS_MODE_16 = 1 << 1          // 16-bit mode
-const CS_MODE_32 = 1 << 2          // 32-bit mode
-const CS_MODE_64 = 1 << 3          // 64-bit mode
-const CS_MODE_THUMB = 1 << 4       // ARM's Thumb mode including Thumb-2
-const CS_MODE_MICRO = 1 << 4       // MicroMips mode (MIPS architecture)
-const CS_MODE_N64 = 1 << 5         // Nintendo-64 mode (MIPS architecture)
-const CS_MODE_SYNTAX_ATT = 1 << 30 // ATT asm syntax (CS_ARCH_X86 architecture)const
-const CS_MODE_BIG_ENDIAN = 1 << 31 // big endian mode
+const (
+	// Engine Architectures
+	CS_ARCH_ARM   = 0 // ARM architecture (including Thumb Thumb-2)
+	CS_ARCH_ARM64 = 1 // ARM-64, also called AArch64
+	CS_ARCH_MIPS  = 2 // Mips architecture
+	CS_ARCH_X86   = 3 // X86 architecture (including x86 & x86-64)
+	CS_ARCH_MAX   = 4
+)
 
 const (
+	// Engine Modes
+	CS_MODE_LITTLE_ENDIAN = 0       // little endian mode (default mode)
+	CS_MODE_SYNTAX_INTEL  = 0       // Intel X86 asm syntax (CS_ARCH_X86 architecture)
+	CS_MODE_ARM           = 0       // 32-bit ARM
+	CS_MODE_16            = 1 << 1  // 16-bit mode
+	CS_MODE_32            = 1 << 2  // 32-bit mode
+	CS_MODE_64            = 1 << 3  // 64-bit mode
+	CS_MODE_THUMB         = 1 << 4  // ARM's Thumb mode including Thumb-2
+	CS_MODE_MICRO         = 1 << 4  // MicroMips mode (MIPS architecture)
+	CS_MODE_N64           = 1 << 5  // Nintendo-64 mode (MIPS architecture)
+	CS_MODE_SYNTAX_ATT    = 1 << 30 // ATT asm syntax (CS_ARCH_X86 architecture)const
+	CS_MODE_BIG_ENDIAN    = 1 << 31 // big endian mode
+)
+
+const (
+	// All type of errors encountered by Capstone API.
+	// These are values returned by cs_errno()
 	CS_ERR_OK     = iota // No error: everything was fine
 	CS_ERR_MEM           // Out-Of-Memory error
 	CS_ERR_ARCH          // Unsupported architecture
 	CS_ERR_HANDLE        // Invalid handle
+	CS_ERR_CSH           // Invalid csh argument
+	CS_ERR_MODE          // Invalid/unsupported mode
 )
