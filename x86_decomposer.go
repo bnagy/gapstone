5aa36ba43112d60960f0d58ad94968ccca3c699f
Roi Martin (@nibble_ds) <jroi.martin@gmail.com>
2013-12-24 19:05:38 +0100

    Fix capstone includes

--- x86_decomposer.go
+++ x86_decomposer.go
@@ -3,7 +3,7 @@ package gapstone
 // #cgo CFLAGS: -I/usr/include/capstone
 // #cgo LDFLAGS: -lcapstone
 // #include <stdlib.h>
-// #include <capstone.h>
+// #include <capstone/capstone.h>
 import "C"
 import "unsafe"
 import "reflect"
