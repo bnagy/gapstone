2456fd016c889d403d6cf4c27c808e1d40ff22f9
ben <git@ben.iagu.net>
2015-06-06 09:28:20 +0700

    back out CI on this branch, derp

--- travis_install_capstone_stable.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -ex
-mkdir -p $HOME/src && cd $HOME/src
-wget https://github.com/aquynh/capstone/archive/3.0.3.tar.gz
-tar -zxvf 3.0.3.tar.gz
-cd capstone-3.0.3 && ./make.sh && sudo make install
-cd $TRAVIS_BUILD_DIR
