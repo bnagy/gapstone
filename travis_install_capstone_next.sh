3db9419f92ce2fe805a3791cbb071c708a3a7322
ben <git@ben.iagu.net>
2015-07-16 13:04:18 +0545

    make git clone faster

--- travis_install_capstone_next.sh
+++ travis_install_capstone_next.sh
@@ -2,7 +2,7 @@
 
 set -ex
 mkdir -p $HOME/src && cd $HOME/src
-git clone https://github.com/aquynh/capstone.git && cd capstone && git checkout next
+git clone --depth=50 --branch=next https://github.com/aquynh/capstone.git && cd capstone
 echo `git log | head`
 make && sudo make install
 cd $TRAVIS_BUILD_DIR
