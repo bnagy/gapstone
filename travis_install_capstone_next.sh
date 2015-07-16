ad60d72d2e2138c61ec94f46b921dd5bf7e099e2
ben <git@ben.iagu.net>
2015-07-16 12:49:20 +0545

    try using make instead of make.sh for travis

--- travis_install_capstone_next.sh
+++ travis_install_capstone_next.sh
@@ -4,5 +4,5 @@ set -ex
 mkdir -p $HOME/src && cd $HOME/src
 git clone https://github.com/aquynh/capstone.git && cd capstone && git checkout next
 echo `git log | head`
-./make.sh && sudo make install
+make && sudo make install
 cd $TRAVIS_BUILD_DIR
