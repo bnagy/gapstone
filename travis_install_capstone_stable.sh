91dd72f56d0bfb099e8b2dcfaa6e3bba0030d9e2
ben <git@ben.iagu.net>
2015-07-17 15:38:41 +0545

    need make as well as make install

--- travis_install_capstone_stable.sh
+++ travis_install_capstone_stable.sh
@@ -4,5 +4,5 @@ set -ex
 mkdir -p $HOME/src && cd $HOME/src
 git clone --depth=50 --branch=3.0.4 https://github.com/aquynh/capstone.git && cd capstone
 echo `git log | head`
-PREFIX=$HOME/capstone make install
+PREFIX=$HOME/capstone make && PREFIX=$HOME/capstone make install
 cd $TRAVIS_BUILD_DIR
