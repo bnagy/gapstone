97a716f56ad48a1c67b5fc7b951d1c9c53cfd379
Scott Knight <4534275+knightsc@users.noreply.github.com>
2019-02-01 16:49:23 -0500

    Update Travis CI build configuration

--- travis_install_capstone_stable.sh
+++ travis_install_capstone_stable.sh
@@ -2,7 +2,7 @@
 
 set -ex
 mkdir -p $HOME/src && cd $HOME/src
-git clone --depth=50 --branch=3.0.4 https://github.com/aquynh/capstone.git && cd capstone
+git clone --depth=50 --branch=4.0.1 https://github.com/aquynh/capstone.git && cd capstone
 echo `git log | head`
 PREFIX=$HOME/capstone make && PREFIX=$HOME/capstone make install
 cd $TRAVIS_BUILD_DIR
